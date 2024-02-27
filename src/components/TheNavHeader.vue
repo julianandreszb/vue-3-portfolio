1a5532b3064b504acf752e0451280e495f1430b8
jzapata <julianandreszb@gmail.com>
2024-02-27 05:07:02 -0500

    style: Modify TheNavHeader.vue to ensure the toggle menu and desktop menu work only using pure CSS

--- src/components/TheNavHeader.vue
+++ src/components/TheNavHeader.vue
@@ -1,64 +1,152 @@
-<script setup lang="ts">
-import NavHeaderToggleButton from '@/components/NavHeaderToggleButton.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <div class="container">
-      <NavHeaderToggleButton />
+  <header class="nav-heaver">
+    <div class="container-toggle-button">
+      <label class="toggle-button">
+        <input type="checkbox" />
+      </label>
     </div>
-    <nav>
-      <a class="text-md-semibold" href="/">Home</a>
-      <a class="text-md-semibold" href="/about">About</a>
-      <a class="text-md-semibold" href="/services">Services</a>
-      <a class="text-md-semibold" href="/contact">Contact</a>
+    <nav class="nav-menu">
+      <a class="nav-menu-item text-md-semibold" href="/">Home</a>
+      <a class="nav-menu-item text-md-semibold" href="/about">About</a>
+      <a class="nav-menu-item text-md-semibold" href="/services">Services</a>
+      <a class="nav-menu-item text-md-semibold" href="/contact">Contact</a>
     </nav>
   </header>
 </template>
 
 <style lang="scss" scoped>
-header {
+.toggle-button {
+  --bar-width: 2.4rem;
+  --bar-height: 0.24rem;
+  --hamburger-gap: 0.5rem;
+  --foreground: #333;
+  --background: white;
+  --hamburger-margin: 0.8rem;
+  --animation-timing: 200ms ease-in-out;
+  --hamburger-height: calc(var(--bar-height) * 3 + var(--hamburger-gap) * 2);
+  --x-width: calc(var(--hamburger-height) * 1.41421356237);
+
+  background-color: var(--bg-primary);
+  border-radius: var(--radius-md);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  gap: var(--hamburger-gap);
+  z-index: 2;
+}
+
+.toggle-button::before,
+.toggle-button::after,
+.toggle-button input {
+  content: '';
+  width: var(--bar-width);
+  height: var(--bar-height);
+  background-color: var(--fg-secondary-700);
+  border-radius: var(--radius-full);
+  transform-origin: left center;
+
+  transition:
+    opacity var(--animation-timing),
+    width var(--animation-timing),
+    rotate var(--animation-timing),
+    translate var(--animation-timing),
+    background-color var(--animation-timing);
+}
+
+.toggle-button input {
+  appearance: none;
+  outline: none;
+  pointer-events: none;
+}
+
+.toggle-button:has(input:checked)::before {
+  rotate: 45deg;
+  width: var(--x-width);
+  translate: 0 calc(var(--bar-height) / -2);
+}
+
+.toggle-button:has(input:checked)::after {
+  rotate: -45deg;
+  width: var(--x-width);
+  translate: 0 calc(var(--bar-height) / 2);
+}
+
+.toggle-button input:checked {
+  opacity: 0;
+  width: 0;
+}
+
+.container-toggle-button:has(input:checked) + .nav-menu {
+  transition: translate 500ms ease-in-out;
+  translate: 0;
+}
+
+.nav-heaver {
+  container-type: inline-size;
   display: flex;
   flex-direction: column;
   justify-content: center;
-
-  .container {
-    height: 7.2rem;
-    display: flex;
-    justify-content: end;
-    padding-inline-start: var(--container-padding-mobile);
-    padding-inline-end: var(--spacing-lg);
-    padding-block: 0;
-    align-items: center;
-  }
 }
 
-nav {
+.container-toggle-button {
+  height: 7.2rem;
+  display: flex;
+  justify-content: end;
+  padding-inline-start: var(--container-padding-mobile);
+  padding-inline-end: var(--spacing-lg);
+  padding-block: 0;
+  align-items: center;
+}
+
+.nav-menu {
   display: flex;
   flex-direction: column;
   padding-block: var(--spacing-3xl);
   gap: var(--spacing-md);
+  transition: translate 500ms ease-in-out;
+  translate: -100%;
+}
 
-  a {
-    color: var(--text-primary-900);
-    padding-block: var(--spacing-lg);
-    padding-inline: var(--spacing-xl);
-    text-decoration: none;
+.nav-menu-item {
+  color: var(--text-primary-900);
+  padding-block: var(--spacing-lg);
+  padding-inline: var(--spacing-xl);
+  text-decoration: none;
 
-    &:hover {
-      background-color: var(--bg-primary_hover);
-    }
+  &:hover {
+    background-color: var(--bg-primary_hover);
   }
 }
 
-//nav {
-//  position: sticky;
-//  top: 0;
-//  display: flex;
-//  justify-content: end;
-//  padding-block: 2.4rem;
-//  padding-inline: 2.4rem;
-//  min-height: 6rem;
-//  z-index: 100;
-//}
+@container (min-width: 768px) {
+  .container-toggle-button {
+    display: none;
+  }
+
+  .nav-menu {
+    translate: 0;
+
+    flex-direction: row;
+    height: 8rem;
+    align-items: center;
+    justify-content: end;
+    padding-inline: var(--container-padding-desktop);
+    gap: var(--spacing-4xl);
+  }
+
+  .nav-menu-item {
+    padding-block: var(--spacing-none);
+    padding-inline: var(--spacing-none);
+    margin-block: var(--spacing-none);
+    margin-inline: var(--spacing-none);
+    color: var(--button-tertiary-fg);
+
+    &:hover {
+      background-color: transparent;
+      color: var(--button-tertiary-fg_hover);
+    }
+  }
+}
 </style>
