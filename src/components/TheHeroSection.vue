94c3b2f3dad3df98b58a62ebaad6262efe57151c
jzapata <julianandreszb@gmail.com>
2024-02-27 23:25:07 -0500

    feat: base hero section content

---
+++ src/components/TheHeroSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import imagePath from '@/assets/images/img_1.png'
+</script>
+
+<template>
+  <section>
+    <img :src="imagePath" alt="" />
+    <div class="hero-description-container">
+      <h2>Clean code, beautiful design, Full-stack mastered</h2>
+      <p>
+        More than 10+ years of experience, fluent in JavaScript, Vue, PHP, and Rust. Bringing ideas
+        to life through web development, from design to deployment.
+      </p>
+      <button>Download CV</button>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
