a455a28c08cdaff6be9319720d60ecabfafbec5b
jzapata <julianandreszb@gmail.com>
2024-03-04 21:08:03 -0500

    feat: Finish hero section, implement VueButton.vue component

--- src/components/TheHeroSection.vue
+++ src/components/TheHeroSection.vue
@@ -1,19 +1,82 @@
 <script setup lang="ts">
-import imagePath from '@/assets/images/img_1.png'
+import imagePath from '@/assets/images/imgHero.png'
+import VueButton from '@/components/VueButton.vue'
 </script>
 
 <template>
-  <section>
-    <img :src="imagePath" alt="" />
-    <div class="hero-description-container">
-      <h2>Clean code, beautiful design, Full-stack mastered</h2>
-      <p>
-        More than 10+ years of experience, fluent in JavaScript, Vue, PHP, and Rust. Bringing ideas
-        to life through web development, from design to deployment.
-      </p>
-      <button>Download CV</button>
-    </div>
-  </section>
+  <div class="section-container hero-container">
+    <section class="hero">
+      <img class="hero-img" :src="imagePath" alt="" />
+      <div class="hero-content-wrapper">
+        <div class="hero-content">
+          <h2 class="hero-heading">Clean code, beautiful design, Full-stack mastered</h2>
+          <p class="hero-supporting-text">
+            More than 10+ years of experience, fluent in JavaScript, Vue, PHP, and Rust. Bringing
+            ideas to life through web development, from design to deployment.
+          </p>
+        </div>
+        <VueButton class="btn-download-cv" hierarchy="Primary" size="xl" state="Default"
+          >Download CV
+        </VueButton>
+      </div>
+    </section>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use 'src/assets/styles/flex';
+@use 'src/assets/styles/text-styles';
+
+.hero-container {
+  container-type: inline-size;
+}
+
+.hero {
+  @include flex.column;
+  padding-block: var(--spacing-4xl);
+  gap: var(--spacing-7xl);
+}
+
+.hero-content-wrapper {
+  @include flex.column-gap-32;
+}
+
+.hero-content {
+  @include flex.column-gap-16;
+}
+
+.hero-heading {
+  @include text-styles.display-md-semibold;
+  color: var(--text-primary-900);
+}
+
+.hero-supporting-text {
+  @include text-styles.text-lg-regular;
+  color: var(--text-tertiary);
+}
+
+@container (min-width: 768px) {
+  .hero {
+    flex-direction: row-reverse;
+  }
+
+  .hero-img {
+    max-width: 50%;
+    object-fit: cover;
+  }
+
+  .hero-heading {
+    @include text-styles.display-lg-semibold;
+  }
+}
+
+@container (min-width: 1280px) {
+  .hero-heading {
+    @include text-styles.display-xl-semibold;
+  }
+
+  .btn-download-cv {
+    width: max-content;
+  }
+}
+</style>
