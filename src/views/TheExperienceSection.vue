32a604f76f894e897d71a06a26c8db1f3e98eb95
Julian Zapata <julianandreszb@gmail.com>
2024-03-13 22:40:43 -0500

    style: implement container query for experience section

--- src/views/TheExperienceSection.vue
+++ src/views/TheExperienceSection.vue
@@ -1,12 +1,38 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import StepBase from '@/components/timeline/StepBase.vue'
 </script>
 
 <template>
   <section class="experience section-container">
-    <StepBase></StepBase>
-    <StepBase></StepBase>
+    <h2 class="experience-heading">Experience</h2>
+    <div>
+      <StepBase></StepBase>
+      <StepBase></StepBase>
+    </div>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped>
+@use 'src/assets/styles/text-styles';
+
+.experience {
+  container-type: inline-size;
+}
+
+.experience-heading {
+  @include text-styles.display-sm-semibold;
+  color: var(--text-primary);
+}
+
+@container (min-width: 768px) {
+  .experience-heading {
+    @include text-styles.display-md-semibold;
+  }
+}
+
+@container (min-width: 1280px) {
+  /*.experience-heading {
+    @include text-styles.display-md-semibold;
+  }*/
+}
+</style>
