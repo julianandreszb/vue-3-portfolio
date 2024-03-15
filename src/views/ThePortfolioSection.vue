343609566e81a13455affe4cffa19c4cdcb46fc0
Julian Zapata <julianandreszb@gmail.com>
2024-03-14 23:04:34 -0500

    feat: create blog-card.vue component base

---
+++ src/views/ThePortfolioSection.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import BlogCard from '@/components/portfolio/blog-card.vue'
+</script>
+
+<template>
+  <BlogCard></BlogCard>
+</template>
+
+<style lang="scss" scoped></style>
