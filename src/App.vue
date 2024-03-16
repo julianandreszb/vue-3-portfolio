cae341a65b83eaa08808ef3b671e924153928770
Julian Zapata <julianandreszb@gmail.com>
2024-03-16 15:14:09 -0500

    feat: Achievements section base

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import TheNavHeader from '@/components/TheNavHeader.vue'
 import TheHeroSection from '@/views/TheHeroSection.vue'
 import TheExperienceSection from '@/views/TheExperienceSection.vue'
+import ThePortfolioSection from '@/views/ThePortfolioSection.vue'
+import TheAchievementsSection from '@/views/TheAchievementsSection.vue'
 </script>
 
 <template>
   <TheNavHeader />
   <TheHeroSection />
   <TheExperienceSection />
+  <ThePortfolioSection />
+  <TheAchievementsSection />
 </template>
 
 <style scoped></style>
---
+++ src/views/TheAchievementsSection.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import iconGitPath from '@/assets/icons/iconGit.png'
+</script>
+
+<template>
+  <section class="section-container achievements">
+    <h2 class="portfolio-heading">Education/Certifications</h2>
+    <section>
+      <article>
+        <img :src="iconGitPath" alt="Git Icon" />
+        <div>
+          <h2 class="achievement-heading">Clean Code</h2>
+          <h3 class="achievement-subheading">Udemy</h3>
+          <h3 class="achievement-subheading-date-range">Issued Jul 2021</h3>
+          <button>Show credential</button>
+        </div>
+      </article>
+    </section>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
